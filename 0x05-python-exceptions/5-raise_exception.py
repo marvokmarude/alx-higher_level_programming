e6471ab9d28f97cfbac4b5f1b3e896298175f9a8
marvokmarude <59051230+marvokmarude@users.noreply.github.com>
2022-07-18 16:00:15 +0100

    send

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-raise_exception.py
+# Author: James Obekpa
+
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Author: James Obekpa
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
